53e9ed50b2ee4d1284a9fe5bc7af316f558a5311
sharikian <wacoinseig@gmail.com>
2026-06-24 22:07:13 +0330

    fix(android) add myket maven repositories

---
+++ frontend/src-tauri/gen/android/build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    repositories {
+        maven { url = uri("https://maven.myket.ir") }
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+    }
+}
+
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.myket.ir") }
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
